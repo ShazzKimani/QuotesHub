74a91670175e449219a32eb1afd30e3ff7ffc2da
Sharon Kimani <sharonkimani01@gmail.com>
2020-08-25 11:13:06 +0300

    Added logic to quote form component ts file

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// import { globalAgent } from 'http';
+// import { Quote } from '@angular/compiler';
+import { Quote} from '../quote';
+// import { from } from 'rxjs';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote=new Quote(0,"","","",new Date(),0,0);
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0,"","","",new Date(),0,0)
+  }
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
